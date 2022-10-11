881ded47934df959039b496fb1f7364c80fe4f5d
Sgt-Foley <112837804+Sgt-Foley@users.noreply.github.com>
2022-10-11 20:22:42 +0800

    P21-P22关键字

---
+++ 2022.10.3-2/2022.10.3-2/1.c
@@ -0,0 +1,62 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//typedef unsigned int u_int;
+//int main()
+//{
+//	unsigned int num = 100;
+//	u_int num2 = 100;//程序中u_int与unsigned int同义
+//	return 0;
+//}
+
+
+//void test()//void表示函数无返回值
+//{
+//	static int a = 1;
+//	a++;
+//	printf("%d\n", a);
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+//extern int g_val;//extern声明外部变量
+//int main()
+//{
+//	printf("%d\n", g_val);
+//	return 0;
+//}
+
+//extern int Add(int,int);//声明函数
+//int main()
+//{
+//	int a = 10, b = 20;
+//	int sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//#define MAX 1000//define定义符号
+//
+//int main()
+//{
+//	printf("%d\n", MAX);
+//	return 0;
+//}
+
+//#define ADD(X,Y) ((X)+(Y))
+//
+//int main()
+//{
+//	printf("%d\n", 4*ADD(2, 3));
+//	return 0;
+//}
