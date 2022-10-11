98c5f16d35e445b7ecaacd78027116300e3ea747
Sgt-Foley <112837804+Sgt-Foley@users.noreply.github.com>
2022-10-11 20:23:40 +0800

    P25初识指针

---
+++ 2022.10.5-1/2022.10.5/1.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 10;
+//	printf("%p\n", &a);
+//	int* pa = &a;
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &pa;
+//	*pa = 20;//*解引用操作，*pa通过pa里的地址找到a 
+//	return 0;
+//}
+
+int main()
+{
+	printf("%d\n", sizeof(int*));
+	printf("%d\n", sizeof(char*));
+	printf("%d\n", sizeof(short*));
+	printf("%d\n", sizeof(long*));
+	printf("%d\n", sizeof(long long*));
+	printf("%d\n", sizeof(float*));
+	printf("%d\n", sizeof(double*));
+	return 0;
+}
