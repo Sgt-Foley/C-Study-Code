a31ec19f2885e9275952b1b5ebdddedbe6a96b20
Sgt-Foley <112837804+Sgt-Foley@users.noreply.github.com>
2022-10-11 20:21:46 +0800

    P19-P20
    鹏哥C语言P19-P20操作符

---
+++ 2022.10.3-1/2022.10.3/源.c
@@ -0,0 +1,79 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()//按位取反
+//{
+//	int a = 0;
+//	printf("%d\n", ~a);
+//	
+//	return 0;
+//}
+
+//int main()//++ / --（自加 / 自减）
+//{
+//	int a = 10;
+//	int b = a++;
+//	printf("%d\n", b);
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+//
+//int main()//(类型)强制类型转换
+//{
+//	int a = (int)3.14;//强制类型转换
+//
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3, b = 0, c = a || b;
+//	printf("%d\n", c);
+//
+//	return 0;
+//}
+
+//int main()//条件操作符
+//{
+//	int a = 0, b = 3, max = 0;
+//	max = a > b ? a : b;
+//	printf("%d\n", max);
+//
+//	return 0;
+//}
+
+//int main()//逗号表达式
+//{
+//	int a = 0;
+//	int b = 3;
+//	int c = 5;
+//	//			a=5			c=1			b=3
+//	int d = (a = b + 2, c = a - 4, b = c + 2);
+//	//逗号表达式从左向右依次计算
+//	//整个表达式结果最后一个表达式结果
+//	printf("%d\n", d);
+//
+//	return 0;
+//}
+
+//int main()//下标引用操作符
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d", arr[5]);
+//	return 0;
+//}
+
+//int main()//调用函数操作符
+//{
+//	printf("hello,are you OK?\n");
+//	printf("%d\n", 100);
+//	return 0;
+//}
+
+//int main()
+//{
+//	register int num = 100;//建议num的值存放在寄存器里
+//	return 0;
+//}
