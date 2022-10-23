da37153a3e822d7c877f74bdc12325678e7fd39c
Sgt-Foley <112837804+Sgt-Foley@users.noreply.github.com>
2022-10-23 10:47:03 +0800

    P26初识结构体

---
+++ 2022.10.18-1/test.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//结构体可以让C语言创建新类型
+struct Stu//创建一个学生结构体
+{
+	char name[20];//名字
+	int age;//年龄
+	double score;//成绩
+};
+
+
+int main()
+{
+	struct Stu s = { "Zhang San",20,80 };//结构体的创建和初始化
+	
+	printf("1:%s %d %lf\n", s.name, s.age, s.score);//"."操作符，"s"为结构体变量，可以找到成员
+	struct Stu * ps = &s;//*ps指针存放结构体s地址
+	printf("2:%s %d %lf\n", (*ps).name, (*ps).age, (*ps).score);
+
+	printf("3:%s %d %lf\n", ps->name, ps->age, ps->score);//"->"结构体指针--成员变量名
+	return 0;//%lf打印双精度浮点型（打印数值巩固）
+}
